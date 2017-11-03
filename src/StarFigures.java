78615fef010ec9ec35052c2da1f0a3f8719c26c9
arballop <33101698+arballop@users.noreply.github.com>
2017-11-02 17:24:27 -0700

    Made algorithm to draw a figure and tested code for prediction

--- src/StarFigures.java
+++ src/StarFigures.java
@@ -1,16 +1,63 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Paysen Arballo
+  	Date: 11/02/2017
 */
 
-// 4. Your pseudocode algorithm for how to break down the figure
+// 4. Your pseudocode algorithm for how to break down the figure:
+//    Use "for" loops to determine how many *'s are produced for each line
+//    Construct loops according to how many *s per line
+//    Use static methods to call upon repeated code for reduction in redundancy.
 
 
 public class StarFigures {
 
 	public static void main(String[] args) {
-
+		drawfigure1();
+		drawfigure2();
+		drawfigure3();
+		drawfigure4();
+		drawfigure5();
+	}
+	public static void drawfigure1() {
+		int sz= 3;
+		//First line "*****"
+		for(int y = 1; y<sz; y++)
+		System.out.println("*****");
+	}
+	public static void drawfigure2() {
+		int sz = 3;
+		//Final lines in first figure    ( " * * "2x)  ("  *  ")
+		for(int y =2; y<sz; y++)
+			System.out.println(" * * ");
+		    System.out.println("  *  " );
+		    System.out.println(" * *");
+		    
+	}
+	public static void drawfigure4() {
+		 int sz=4; 
+			    for(int y = 1; y<sz; y++ )      
+					System.out.println("  *  ");// Gives the beginning for next figure. 3 lines *
+	}
+	public static void drawfigure3() {
+		System.out.println("\n");// Sets new line for next figure.
+		drawfigure1();          // Calls for previously used code "*****"
+		drawfigure2();         // Calls for previously used code 
+		drawfigure1();        // Calls for previously used code
+		System.out.println("\n");// Sets new line for next figure
+		}
+		public static void drawfigure5() {
+			//Final Figure
+		    	//drawfigure4();//Calls for previously used code to complete figure
+		    	drawfigure1();//Calls for previously used code to complete figure
+		    	drawfigure2();//Calls for previously used code to complete figure
+		    	}
+		    
+		   private static String String(String s, int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+		
 	}
 
-}
--- src/Tricky.java
+++ src/Tricky.java
@@ -1,17 +1,41 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Paysen Arballo
+  	Date: 11/02/2017
 */
 
-// 1. Your predicted output 
+// 1. Your predicted output: " Done with main."
+//                           " This is message1."
+//                           " This is message2."
+//                           " Done with message2."
  
 
 public class Tricky {
+	 public static void main(String[] args) {
+	        message1();
+	        message2();
+	        System.out.println("Done with main.");
+	    }
+	    
+	    public static void message1() {
+	        System.out.println("This is message1.");
+	    }
+	    
+	    public static void message2() {
+	        System.out.println("This is message2.");
+	        message1();
+	        System.out.println("Done with message 2.");
+	    }
+	}
 
-}
 
-// 3. Were you correct? Explain any differences
+
+
+// 3. Were you correct? Explain any differences: 
+//    I was not correct, however I got to see how the loop worked after I ran it and it showed me exactly how the loop runs.
+//    The differences were present because I overlooked the process and went straight through the loop reading, instead of 
+//    looking at details to map out the output. I now know how to map out the output just by looking at the code. My output 
+//    prediction was not accurate but I do not believe I was that far off.
  
  
 
